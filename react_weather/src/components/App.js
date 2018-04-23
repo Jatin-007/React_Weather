d6e275ed20264cb9eeaaf4abb760e4a7ea85a7ff
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-23 03:12:32 -0700

    SearchBar

--- react_weather/src/components/App.js
+++ react_weather/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <SearchBar/>
       </div>
     );
   }
