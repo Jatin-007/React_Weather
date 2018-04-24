bf28f6c163102908b5c52693f9a9110f5a735c31
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-24 02:03:32 -0700

    Integrating Middlewares

--- react_weather/src/containers/SearchBar.js
+++ react_weather/src/containers/SearchBar.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state = {term: ''};
 
-        this.onInputChange = this.onInputChange.bind(this);
+        this.state = {term: ''}; // initial value is empty
+        this.onInputChange = this.onInputChange.bind(this); // in this(searchBar component) find the onInputChange method and bind it with this
     }
 
     onInputChange(event) {
         this.setState({term: event.target.value});
     }
 
+    onFormSubmit(event){
+        event.preventDefault();
+
+        // need to add the weather data
+    } 
 
     render () {
         return (
             <div>
-                <form className="input-group">
-                    <input
-                        placeholder="Get a 5-day forcast in your favourite city"
+                <form onSubmit= {this.onFormSubmit} className="input-group">
+                    <input 
+                        placeholder ="Get a five day forcast in your favourite cities"
                         className="form-control"
-                        value={this.state.term}    
-                        onChange = {this.onInputChange}
+                        value = {this.state.term}
+                        onChange={this.onInputChange}
                     />
-
                     <span className="input-group-btn">
                         <button type="submit" className="btn btn-secondary">Submit</button>
                     </span>
