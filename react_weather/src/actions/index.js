a59fea2e36cf068d77331b4b4f5579f16d1ca6c0
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-13 13:30:31 -0700

    simple reducer

--- react_weather/src/actions/index.js
+++ react_weather/src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const API_KEY = 'b265236f90d5a85243173d8fca43bc17'; // openweathermap.org API key
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER'; // to keep our action types consistent
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city}`;
