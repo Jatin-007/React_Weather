a55cacbb41b7ab0a61d388159fcdbe13de29c63a
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-13 15:47:32 -0700

    Weather List + connected reducer

--- react_weather/src/components/App.js
+++ react_weather/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/SearchBar';
+import WeatherList from '../containers/WeatherList';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <SearchBar/>
+        <SearchBar />
+        <WeatherList />
       </div>
     );
   }
