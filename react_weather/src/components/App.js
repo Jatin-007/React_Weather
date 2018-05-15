c81280852c854ac428b8a519964acfc166523aa8
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-15 01:51:01 -0700

    few stylings

--- react_weather/src/components/App.js
+++ react_weather/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/SearchBar';
 import WeatherList from '../containers/WeatherList';
+import styles from '../style/styles.css';
 
 class App extends Component {
   render() {
