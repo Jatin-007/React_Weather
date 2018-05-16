6307cc95bc64cb69bce58a2d324719da584ed02b
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-05-15 19:14:27 -0700

    google maps

---
+++ react_weather/src/components/google_map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GoogleMap extends Component {
+  render() {
+    return (
+      <div>
+        Google Map API
+      </div>
+    )
+  }
+}
+
+export default GoogleMap;
